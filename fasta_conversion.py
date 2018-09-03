34757353683f4de0a4be54e95eba4ef7a4f68ceb
Alison Maxwell <alimichellemax@gmail.com>
2018-09-03 13:14:54 -0700

    hamming distances added

--- fasta_conversion.py
+++ fasta_conversion.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 f = open('CTGATC.fastq')
 
 lines_list = f.readlines()
@@ -10,4 +12,23 @@ while count < len(lines_list)-1:
 		if possible_sequence[0:1] != '@':
 			seqs_list.append(possible_sequence)
 	count +=1
-print seqs_list
+#print seqs_list
+
+def hamdist(str1, str2):
+   """Count the # of differences between equal length strings str1 and str2"""
+   diffs = 0
+   for ch1, ch2 in zip(str1, str2):
+       if ch1 != ch2:
+           diffs += 1
+   return diffs
+
+ham_count = 0
+distances = []
+while ham_count < len(seqs_list)-1:
+	i = seqs_list[ham_count]
+	j = seqs_list[ham_count+1]
+	distances.append(hamdist(i,j))
+	ham_count += 1 
+
+plt.hist(distances, bins = 10)
+plt.show()
